d6f82d282dd5b42177a93ef18737db59abe900f5
Calr Micheal Acot <calrmichealacot@gmail.com>
2023-04-17 16:55:31 +0800

    adding Method and put comments button

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def index
+    @comments = @post.comments
+  end
+
+  private
+
+  def set_post
+    @post = Post.find params[:post_id]
+  end
+end
+
