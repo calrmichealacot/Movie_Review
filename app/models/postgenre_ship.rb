a52f5f8b8520206cbe4e3afe3550bbcce7e59fd6
Calr Micheal Acot <calrmichealacot@gmail.com>
2023-04-17 18:09:11 +0800

    movie review

--- app/models/postgenre_ship.rb
+++
@@ -1,2 +0,0 @@
-class PostgenreShip < ApplicationRecord
-end
--- db/migrate/20230417094206_create_postgenre_ships.rb
+++
@@ -1,9 +0,0 @@
-class CreatePostgenreShips < ActiveRecord::Migration[7.0]
-  def change
-    create_table :postgenre_ships do |t|
-      t.references :post
-      t.references :genre
-      t.timestamps
-    end
-  end
-end
