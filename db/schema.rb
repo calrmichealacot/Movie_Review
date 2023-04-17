fe57679258f81992aab01d0177e80279bd62bf04
Calr Micheal Acot <calrmichealacot@gmail.com>
2023-04-17 18:02:38 +0800

    create category/genre and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_094206) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_095856) do
+  create_table "categories", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.bigint "post_id", null: false
@@ -19,19 +25,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_094206) do
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
-  create_table "genres", charset: "utf8mb4", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "postgenre_ships", charset: "utf8mb4", force: :cascade do |t|
+  create_table "post_category_ships", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "post_id"
-    t.bigint "genre_id"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["genre_id"], name: "index_postgenre_ships_on_genre_id"
-    t.index ["post_id"], name: "index_postgenre_ships_on_post_id"
+    t.index ["category_id"], name: "index_post_category_ships_on_category_id"
+    t.index ["post_id"], name: "index_post_category_ships_on_post_id"
   end
 
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
