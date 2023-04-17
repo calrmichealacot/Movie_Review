c7fe97130bdca803167f9a73cf51151418d8b131
Calr Micheal Acot <calrmichealacot@gmail.com>
2023-04-17 17:43:44 +0800

    Creating genre and postgenre table and migrate

---
+++ app/models/postgenre_ship.rb
@@ -0,0 +1,2 @@
+class PostgenreShip < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_084124) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_094206) do
   create_table "comments", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "genres", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "postgenre_ships", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "genre_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["genre_id"], name: "index_postgenre_ships_on_genre_id"
+    t.index ["post_id"], name: "index_postgenre_ships_on_post_id"
   end
 
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
